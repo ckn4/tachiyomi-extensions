e809f08b225071461e9693ae66fd1679c0656f8e
ckn <22233906+ckn4@users.noreply.github.com>
2021-05-08 20:28:25 +0800

    delete multisrc

--- multisrc/overrides/mmrcms/animaregia/src/AnimaRegia.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.pt.animaregia
-
-import eu.kanade.tachiyomi.multisrc.mmrcms.MMRCMS
-
-class AnimaRegia : MMRCMS("AnimaRegia", "https://animaregia.net", "pt-BR"){
-    override val id: Long = 4378659695320121364
-}
