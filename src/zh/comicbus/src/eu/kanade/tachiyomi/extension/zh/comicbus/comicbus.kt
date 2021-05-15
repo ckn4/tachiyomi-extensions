fe8a8820e5ec95dc1319958b1beca381f9aa2fb7
ckn <22233906+ckn4@users.noreply.github.com>
2021-05-16 04:17:12 +0800

    finish comicbus

--- src/zh/comicbus/src/eu/kanade/tachiyomi/extension/zh/comicbus/comicbus.kt
+++ src/zh/comicbus/src/eu/kanade/tachiyomi/extension/zh/comicbus/comicbus.kt
@@ -9,10 +9,11 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.Request
 import okhttp3.Response
 import java.net.URLEncoder
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class comicbus : HttpSource() {
     override val name: String = "comicbus"
@@ -20,24 +21,44 @@ class comicbus : HttpSource() {
     override val supportsLatest: Boolean = true
     override val baseUrl: String = "https://m.comicbus.com"
 
+    private var cid = ""
+    private var path = ""
+
+    override fun headersBuilder() = super.headersBuilder()
+        .add("Referer", baseUrl)
+
     // Popular
 
     override fun popularMangaRequest(page: Int): Request {
-        return GET("$baseUrl/list/click/?page=$page", headers)
+        return GET("http://app.6comic.com:88/i/hot-$page.html", headers)
     }
 
     override fun popularMangaParse(response: Response): MangasPage {
-        TODO("Not yet implemented")
+        return latestUpdatesParse(response)
     }
 
     // Latest
 
     override fun latestUpdatesRequest(page: Int): Request {
-        return GET("$baseUrl/list/update/?page=$page", headers)
+        return GET("http://app.6comic.com:88/i/update-$page.html", headers)
     }
 
     override fun latestUpdatesParse(response: Response): MangasPage {
-        TODO("Not yet implemented")
+        val html = bodyWithCharset(response)
+        val result: Array<String> = html.split("\\|".toRegex()).toTypedArray()
+        val mangas = mutableListOf<SManga>()
+        for (i in result.indices) {
+            val _url = match("(\\d+),,.*?,.*?,([^,]+).*?", result[i], 1)
+            val _title = match("(\\d+),,.*?,.*?,([^,]+).*?", result[i], 2)
+            mangas.add(
+                SManga.create().apply {
+                    url = _url!!
+                    title = _title!!
+                    thumbnail_url = "http://app.6comic.com:88/pics/0/$url.jpg"
+                }
+            )
+        }
+        return MangasPage(mangas, false)
     }
 
     // Search
@@ -68,21 +89,24 @@ class comicbus : HttpSource() {
     override fun mangaDetailsRequest(manga: SManga): Request {
         return GET("http://app.6comic.com:88/info/" + manga.url + ".html")
     }
-//    override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
-//        val _result = emptyArray<String>()
-//        for ((index, i) in document.toString().split("\\|").withIndex())
-//            _result[index] = i
-//        title = _result[4]
-//        thumbnail_url = "http://app.6comic.com:88/pics/0/" + _result[1] + ".jpg"
-//        description = _result[10].substring(2).trim()
-//        status = when (_result[7]) {
-//            "完結" -> SManga.COMPLETED
-//            else -> SManga.ONGOING
-//        }
-//    }
 
     override fun mangaDetailsParse(response: Response): SManga {
-        TODO("Not yet implemented")
+        val result = bodyWithCharset(response)
+        val _result = result.split("\\|".toRegex()).toTypedArray()
+        return SManga.create().apply {
+            title = _result[4]
+            thumbnail_url = "http://app.6comic.com:88/pics/0/" + _result[1] + ".jpg"
+            description = _result[10].substring(2).trim()
+            if (_result[8].indexOf("&#") == -1) {
+                author = _result[8]
+            }
+            genre = _result[9].replace("\\s\\d+:\\d+:\\d+".toRegex(), "")
+            status = when (_result[7]) {
+                "完" -> SManga.COMPLETED
+                "連載中" -> SManga.ONGOING
+                else -> SManga.UNKNOWN
+            }
+        }
     }
 
     // Chapters
@@ -92,27 +116,65 @@ class comicbus : HttpSource() {
     }
 
     override fun chapterListParse(response: Response): List<SChapter> {
-        TODO("Not yet implemented")
+        var result = bodyWithCharset(response)
+        var ourl = response.request.url.toString()
+        ourl = ourl.substring(31, ourl.length - 5)
+        if (match("(<!--ch-->).+", result, 1) != null) {
+            result = result.replace("<!--ch-->".toRegex(), "|")
+        }
+        if (result.indexOf("|") == 2) {
+            result = result.substring(3)
+        }
+        val chapters = mutableListOf<SChapter>()
+        if (result.contains("|")) {
+            val _result = result.split("\\|".toRegex()).toTypedArray()
+            for (m in _result.indices) {
+                val _name = match("\\d+ (.*)", _result[m], 1)
+                if (_name != null) {
+                    chapters.add(
+                        SChapter.create().apply {
+                            name = _name
+                            url = m.toString()
+                            scanlator = ourl
+                        }
+                    )
+                }
+            }
+        } else {
+            chapters.add(
+                SChapter.create().apply {
+                    name = "为动画"
+                    url = ""
+                }
+            )
+        }
+        chapters.reverse()
+        return chapters
     }
 
     // Pages
-
     override fun pageListRequest(chapter: SChapter): Request {
-        val url = (baseUrl + chapter.url).toHttpUrlOrNull()!!.newBuilder()
-            .addQueryParameter("netType", "4")
-            .addQueryParameter("loadreal", "1")
-            .addQueryParameter("imageQuality", "2")
-            .build()
-        return GET(url.toString())
+        cid = chapter.scanlator!!
+        path = chapter.url
+        return GET("http://app.6comic.com:88/comics/" + chapter.scanlator + ".html")
     }
 
-    override fun pageListParse(response: Response): List<Page> = mutableListOf<Page>().apply {
-        val script = ""
-        val images = script.substringAfter("chapterImages = [\"").substringBefore("\"]").split("\",\"")
-        val path = script.substringAfter("chapterPath = \"").substringBefore("\";")
-        val server = script.substringAfter("pageImage = \"").substringBefore("/images/cover")
-        images.forEach {
-            add(Page(size, "", "$server/$path/$it"))
+    override fun pageListParse(response: Response): List<Page> {
+        val html = bodyWithCharset(response)
+        val rresult: Array<String> = html.split("\\|".toRegex()).toTypedArray()
+        val num: Int = path.toInt()
+        val result = rresult[num]
+        val r = result.split(" ".toRegex()).toTypedArray()
+        val name = r[0]
+        val imgserver = r[1]
+        val name1 = r[2]
+        val pagenum = r[3].toInt()
+        val last = r[4]
+        return mutableListOf<Page>().apply {
+            for (i in 0..pagenum) {
+                val last1 = last.substring(i % 100 / 10 + 3 * (i % 10), i % 100 / 10 + 3 + 3 * (i % 10))
+                add(Page(size, "", "http://img" + imgserver + ".8comic.com/" + name1 + "/" + cid + "/" + name + "/" + String.format("%03d", i + 1) + "_" + last1 + ".jpg"))
+            }
         }
     }
 
@@ -134,5 +196,27 @@ class comicbus : HttpSource() {
         } else {
             Log.d(tag, content)
         }
+    }
+
+    // String match
+
+    fun match(regex: String, input: String, group: Int): String? {
+        try {
+            val pattern: Pattern = Pattern.compile(regex)
+            val matcher: Matcher = pattern.matcher(input)
+            if (matcher.find()) {
+                return matcher.group(group)!!.trim()
+            }
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    // decode(big5)
+
+    fun bodyWithCharset(response: Response): String {
+        val htmlBytes: ByteArray = response.body!!.bytes()
+        return String(htmlBytes, charset("big5"))
     }
 }
